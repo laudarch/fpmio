da8d065a6dcc3d42b7f806836eadd1a51a966a1e
fpm2014 <fpm2014@users.noreply.github.com>
2014-11-30 20:20:36 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -5,9 +5,7 @@ echo Search directory...
 if not exist c:\masm32 md c:\masm32
 if not exist c:\masm32\include\ md c:\masm32\include\
 
-if exist c:\masm32\include\io.asm ren c:\masm32\include\io.asm 
-
-io.asm.bak
+if exist c:\masm32\include\io.asm ren c:\masm32\include\io.asm io.asm.bak
 
 echo Copying files...
 copy %cd%\io.asm c:\masm32\include\
